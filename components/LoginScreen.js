872440aa37280bfa6bf1f94453496560f4dbc00e
Anh T Le <ahtle@stanford.edu>
2018-06-13 15:33:38 -0700

    sign-up in progress

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,31 +1,68 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+
 
 export default class LoginScreen extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            email :'',
-            validated: false ,
+            first_name: '',
+            email:'',
+            password: '',
+            LoginOrSignUp: 'Login',
+            showErrorMessage: false,
+            errorMessage: ''
         }
     };
 
-    _onSignInPress() {
-        this.props.navigation.navigate('Dashboard');
+    toggleLogin() {
+        if (this.state.LoginOrSignUp === 'Sign-up') {
+            this.setState({LoginOrSignUp: 'Login', showErrorMessage: false, errorMessage: ''});
+        } else {
+            this.setState({LoginOrSignUp: 'Sign-up'});
+        }
     }
 
-    _validateEmail() {
+    onSubmit() {
+        console.log(this.state);
+        if (this.state.LoginOrSignUp === 'Login') {
+            this.props.navigation.navigate('Dashboard');
+        } else {
+            if (this.state.first_name === '' || this.state.email === '' || this.state.password === '') {
+                this.setState({showErrorMessage: true, errorMessage: 'Please fill out all fields'});
+            } else if (!this.validateEmail) {
+                this.setState({showErrorMessage: true, errorMessage: 'Please fill out all fields'});
+            }
+        }
+    }
+
+    validateEmail() {
         let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
 
-        if (reg.test(text) === false) {
+        if (reg.test(this.state.email) === false) {
             console.log("Email is Not Correct");
-            this.setState({email:text})
             return false;
         } else {
-            this.setState({email:text})
             console.log("Email is Correct");
             return true;
+        }
+    }
+
+    renderNameInput() {
+        if (this.state.LoginOrSignUp === 'Sign-up') {
+            return (
+                <TextInput style={styles.textInput} onChangeText={(first_name) => this.setState({first_name})} placeholder='First Name' keyboardType='email-address' />
+            );
+        } else {
+            return null;
+        }
+    }
+
+    renderSignUpError() {
+        if (this.state.showErrorMessage) {
+            return (<Text style={styles.errorMessage}>{this.state.errorMessage}</Text>);
+        } else {
+
         }
     }
 
@@ -36,11 +73,17 @@ export default class LoginScreen extends React.Component {
                 <View style={{flex: 0.5}} />
 
                 <View style={{flex: 0.4}}>
-                    <TextInput style={styles.textInput} placeholder='Username' keyboardType='email-address'/>
-                    <TextInput style={styles.textInput} placeholder='Password' secureTextEntry={true}/>
+                    {this.renderSignUpError()}
+                    {this.renderNameInput()}
+                    <TextInput style={styles.textInput} onChangeText={(email) => this.setState({email})} placeholder='Email' keyboardType='email-address'/>
+                    <TextInput style={styles.textInput} onChangeText={(password) => this.setState({password})} placeholder='Password' secureTextEntry={true}/>
 
-                    <TouchableOpacity style={styles.button} onPress={() => {this._onSignInPress()}}>
-                        <Text style={{fontSize: 20, color: '#fff'}}>Login</Text>
+                    <TouchableOpacity style={styles.button} onPress={() => {this.onSubmit()}}>
+                        <Text style={{fontSize: 20, color: '#fff'}}>{this.state.LoginOrSignUp}</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {this.toggleLogin()}} >
+                        <Text style={styles.signUp}>{this.state.LoginOrSignUp === 'Login' ? 'Sign-up' : 'Login'}</Text>
                     </TouchableOpacity>
                 </View>
 
@@ -62,10 +105,8 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         paddingBottom: 10,
         paddingLeft: 10,
-        borderBottomColor: '#AC3931',
-        borderBottomWidth: 2,
+        borderBottomWidth: 1,
         fontSize: 20,
-        color: '#AC3931',
     },
     button: {
         padding: 15,
@@ -76,5 +117,16 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 2, height: 1 },
         shadowOpacity: 0.5,
         shadowRadius: 1,
+    },
+    signUp: {
+        color: '#4285f4',
+        textAlign: 'center',
+        fontSize: 16,
+        marginTop: 20
+    },
+    errorMessage: {
+        color: '#b1040e',
+        textAlign: 'center',
+        marginBottom: 10
     }
 });
